1b01bf84bab557a63a5d73c6bfc57f7c2a07081a
mini_136 <blovak.l@gmail.com>
2026-04-01 00:01:50 +0200

    ddfassa

--- run_all_windows.bat
+++ run_all_windows.bat
@@ -3,23 +3,23 @@ REM Aktivace virtuálního prostředí
 call .venv\Scripts\activate
 
 REM Instalace závislostí backendu
-pip install -r app\backend\requirements-backend.txt
+pip install -r backend\requirements-backend.txt
 
 REM Instalace závislostí frontend (pokud je potřeba)
-cd app\frontend
+cd frontend
 if exist node_modules (
     echo Node modules already installed.
 ) else (
     npm install
 )
-cd ..\..
+cd ..
 
 REM Spuštění backendu
-start "backend" cmd /k "python -m uvicorn app.backend.app:app --host localhost --port 40150"
+start "backend" cmd /k "python -m uvicorn backend.app:app --host localhost --port 40150"
 
 REM Spuštění frontendu
-cd app\frontend
+cd frontend
 start "frontend" cmd /k "npx serve public -l 3000"
-cd ..\..
+cd ..
 
 echo Backend běží na http://localhost:40150
